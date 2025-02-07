e6b803f76b4cc7d6947ce26858ccee7294a2627f
g4m4 <misept.dieseneuf@gmail.com>
2025-02-07 18:40:45 +0100

    [CI] Add IWYU pass

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -1,3 +1,29 @@
+# Ancillary function retrieving direct sources absolute file path for direct
+# library dependencies (no recursion) for the given target
+function(get_source_files target_name out_files)
+  set(found_files "")
+  if(target_name)
+    get_target_property(target_source_dir ${target_name} SOURCE_DIR)
+    get_target_property(target_sources ${target_name} SOURCES)
+    foreach(target_source IN ITEMS ${target_sources})
+      cmake_path(SET source_path ${target_source})
+      cmake_path(GET source_path EXTENSION source_extension)
+      cmake_path(
+        ABSOLUTE_PATH
+        source_path
+        BASE_DIRECTORY
+        ${target_source_dir}
+        NORMALIZE
+        OUTPUT_VARIABLE
+        absolute_path)
+      list(APPEND found_files "${absolute_path}")
+    endforeach()
+  endif()
+  set(${out_files}
+      "${found_files}"
+      PARENT_SCOPE)
+endfunction()
+
 function(create_analysis_target target_name)
   add_custom_target(${target_name}_analysis DEPENDS ${target_name}_clang_tidy)
 
@@ -11,5 +37,28 @@ function(create_analysis_target target_name)
       "-p${CMAKE_BINARY_DIR}" "-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy"
       "-header-filter=.*sandbox(?!.*_deps|.*cpm_cache).*"
       "-source-filter=.*sandbox(?!.*_deps|.*cpm_cache).*"
+    VERBATIM USES_TERMINAL)
+
+  # Include what you use
+  find_program(IWYU_COMMAND NAMES iwyu)
+  # We feed it the compilation database, however we filter it to direct sources
+  # for direct library dependencies (no recursion) for every target
+  list(APPEND ${target_name}_sources)
+  get_target_property(${target_name}_libs ${target_name}
+                      INTERFACE_LINK_LIBRARIES)
+  foreach(library IN ITEMS ${${target_name}_libs})
+    set(library_sources "")
+    get_source_files(${library} library_sources)
+    list(APPEND ${target_name}_sources ${library_sources})
+  endforeach()
+  get_source_files(${target_name} target_sources)
+  list(APPEND ${target_name}_sources ${target_sources})
+
+  # We emit clang "errors" so they can be caught easily in the output log
+  # (as other passes will rather emit warnings)
+  add_custom_target(
+    ${target_name}_iwyu
+    COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/iwyu_tool.py
+            -p${CMAKE_BINARY_DIR} ${${target_name}_sources} -o clang-warning
     VERBATIM USES_TERMINAL)
 endfunction()
--- scripts/analyse_log.sh
+++ scripts/analyse_log.sh
@@ -8,7 +8,7 @@ set -e
 
 echo "Analysing log"
 
-grep -e "warning:" -e "should add" -e "should remove" -i $1 | tee $2
+grep -e "warning:" -e "error:" -i $1 | tee $2
 
 if [ -s $2 ]; then
     warnings_count=$(cat $2 | wc -l)
