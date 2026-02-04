01fafeb17bdbf547246e745933cd28f203d74330
gama <misept.dieseneuf@gmail.com>
2026-02-04 14:23:26 +0300

    [CMAKE] Minor reformatting

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -9,30 +9,23 @@ function(setup_dependencies)
     cpmaddpackage(GITHUB_REPOSITORY doctest/doctest GIT_TAG v2.4.11)
   endif()
 
-  # cmake-format: off
   if(${SANDBOX_ENABLE_UI})
-      if(NOT ${SANDBOX_BUILD_ANDROID})
-    # GLFW is required on desktop
-    cpmaddpackage(
-      GITHUB_REPOSITORY
-      glfw/glfw
-      GIT_TAG
-      3.4
-      OPTIONS
-      "GLFW_BUILD_EXAMPLES OFF"
-      "GLFW_BUILD_TESTS OFF"
-      "GLFW_BUILD_DOCS OFF"
-      "GLFW_INSTALL OFF")
-     endif()
+    if(NOT ${SANDBOX_BUILD_ANDROID})
+      # GLFW is required on desktop
+      cpmaddpackage(
+        GITHUB_REPOSITORY
+        glfw/glfw
+        GIT_TAG
+        3.4
+        OPTIONS
+        "GLFW_BUILD_EXAMPLES OFF"
+        "GLFW_BUILD_TESTS OFF"
+        "GLFW_BUILD_DOCS OFF"
+        "GLFW_INSTALL OFF")
+    endif()
 
     # imgui
-    cpmaddpackage(
-      GITHUB_REPOSITORY
-      ocornut/imgui
-      GIT_TAG
-      v1.92.5)
-
-    # cmake-format: on
+    cpmaddpackage(GITHUB_REPOSITORY ocornut/imgui GIT_TAG v1.92.5)
 
     if(imgui_ADDED)
       add_library(imgui)
@@ -40,7 +33,7 @@ function(setup_dependencies)
 
       target_include_directories(
         imgui PUBLIC $<BUILD_INTERFACE:${imgui_SOURCE_DIR}>
-                      $<BUILD_INTERFACE:${imgui_SOURCE_DIR}>/backends)
+                     $<BUILD_INTERFACE:${imgui_SOURCE_DIR}>/backends)
 
       # if(UNIX) find_package(Threads REQUIRED) target_link_libraries(miniaudio
       # PUBLIC ${CMAKE_DL_LIBS} ${CMAKE_THREAD_LIBS_INIT}) endif()
